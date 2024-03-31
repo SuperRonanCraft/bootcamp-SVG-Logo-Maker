2f7bc05e2ae5e2e42eca712b7c75939b741ce463
RonanCraft <SuperRonanCraft@gmail.com>
2024-03-31 00:33:30 -0400

    shapes generate

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-const { Triangle } = require("./lib/shapes");
+// const inquirer = require("inquirer");
+const fs = require("fs");
+const { Triangle, Circle, Square } = require("./lib/shapes");
 
-console.log(new Triangle("text", "red", "blue").createShape());
+//Get back a shape object
+function getShape(shapeText, { text, color, textColor }) {
+  switch (shapeText) {
+    case "square":
+      return new Square(text, color, textColor);
+    case "circle":
+      return new Circle(text, color, textColor);
+    case "triangle":
+      return new Triangle(text, color, textColor);
+  }
+}
+
+//Create and save SVG logo file
+function createSvg(shape) {
+  const svgTxt = shape.createShape();
+  fs.writeFile("./examples/logo.svg", svgTxt, () => {
+    console.log("File created!");
+  });
+}
